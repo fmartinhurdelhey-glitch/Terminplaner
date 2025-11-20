1b8a3394a20f7671ccb8491b173bca2d656651ff
Fabian Martin Hurdelhey <fabianmartinhurdelhey@mbp-von-fabian.speedport.ip>
2025-11-20 19:14:02 +0100

    Set default theme to light mode

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -8,7 +8,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ClientOnly>
       <AuthProvider>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
         </ThemeProvider>
       </AuthProvider>
